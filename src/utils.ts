560107394c07d6eb12ac0e7856c552397a314ffd
Chethan Bhat <chethan@adaptive.live>
2023-06-13 19:20:28 +0530

    Test Env Variables

--- src/utils.ts
+++ src/utils.ts
@@ -9,5 +9,7 @@ export const sanityClient = createClient({
   token: import.meta.env.VITE_SANITY_API_TOKEN,
 });
 
+console.log("TEST");
+
 const builder = imageUrlBuilder(sanityClient);
 export const urlFor = (source: any) => builder.image(source);
